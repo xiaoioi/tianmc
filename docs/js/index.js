99bf1e65dde49470fc8d3487fc45035d953f4e95
root <root@localhost>
2023-01-29 22:09:24 +0800

    主页

---
+++ docs/js/index.js
@@ -0,0 +1,34 @@
+const { createApp, ref, watchEffect } = Vue
+const APPS_URL = "index.json"
+app = createApp({
+    data() {
+        const config = ref(null)
+        fetch(APPS_URL)
+            .then((res) => res.json())
+            .then((json) => (config.value = json))
+            .catch(function (error) {
+                console.error(error)
+            })
+        return {
+            config
+        }
+    },
+    mounted() {
+
+    },
+    updated() {
+        mdui.mutation()
+    }
+})
+mdui.$(function () {
+    app.mount('#app')
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceWorker.js', { scope: '/' }).then(function (reg) {
+            // registration worked
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        }).catch(function (error) {
+            // registration failed
+            console.log('Registration failed with ' + error);
+        });
+    }
+})
